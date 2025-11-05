cf1795a22ac24f7f8a25776c511c8924be3a74b9
Giovanni Coronel <elvis.coronel@unmsm.edu.pe>
2025-11-04 23:44:30 -0500

    add:aiSlice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,10 +3,12 @@ import { createRecipeSlice, RecipesSliceType } from './recipeSlice';
 import { devtools } from 'zustand/middleware';
 import { createFavoritesSlice, FavoriteSliceType } from './favoriteSlice';
 import { createNotificationSlice, NotificationSliceType } from './notificationSlice';
-export const useAppStore = create<RecipesSliceType & FavoriteSliceType & NotificationSliceType>()(devtools((...a)=>({
+import { createAISlice, AISlice } from './aiSlice';
+export const useAppStore = create<RecipesSliceType & FavoriteSliceType & NotificationSliceType & AISlice>()(devtools((...a)=>({
   // de esta forma se conecta al store el slice de recipeSlice y se le pasa los argumentos set , get(para acceder al estado actual del store sin provocar una actualización (re-render) en los componentes) y api( Herramientas avanzadas para manipular el store fuera de React)
   ...createRecipeSlice(...a),
   ...createFavoritesSlice(...a),
-  ...createNotificationSlice(...a)
+  ...createNotificationSlice(...a),
+  ...createAISlice(...a)
   }
 )))
